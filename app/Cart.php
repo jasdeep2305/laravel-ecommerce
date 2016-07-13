ed201e1d118cf625675e4ff7d9662f077f419445
Himanshu Pal <pal.himanshu1991@gmail.com>
2016-07-13 19:24:18 +0530

    Fixed products relation in Cart Model

--- app/Cart.php
+++ app/Cart.php
@@ -9,19 +9,24 @@ class Cart extends Model
     protected $fillable = [
         'user_id'
     ];
-	
+
     public function user()
     {
-    	return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User');
     }
 
-     public function cart_products()
-     {
-     	return $this->hasMany('App\CartProduct');
-     }
+    public function cart_products()
+    {
+        return $this->hasMany('App\CartProduct');
+    }
 
+    /**
+     * Return all products in cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
     public function products()
     {
-        return $this->hasManyThrough('App\Product','App\CartProduct', 'cart_id', 'id');
+        return $this->hasManyThrough('App\Product', 'App\CartProduct', 'cart_id', 'id');
     }
 }
