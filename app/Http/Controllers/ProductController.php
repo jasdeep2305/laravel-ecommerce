20e6cb131753445fe6396a45257e06c9f00b3502
Jasdeep2305 <jasdeeppassion@gmail.com>
2016-07-13 15:01:51 +0530

    renamed product show

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use app\Http\Repositories\ProductRepository;
+use App\Http\Repositories\ProductRepository;
+use App\Product;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
