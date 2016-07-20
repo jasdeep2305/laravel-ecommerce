2cc656949532fd064a4c6ffd4e324627d0a9a774
divyadawra <divyadawra.07@gmail.com>
2016-07-20 11:02:46 +0530

    OrderProductRepository file removed

--- app/Http/Repositories/ProductRepository.php
+++ app/Http/Repositories/ProductRepository.php
@@ -49,7 +49,7 @@ class ProductRepository implements Repository
         $params = $this->params($request);
         $this->uploadFile($request->file('product_image'));
         $product = Product::create($params);
-        event(new NewProductCreated($product));
+       // event(new NewProductCreated($product));
         return $product;
     }
 
